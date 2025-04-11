bd419e50be224b478775f63a9c332f710ce1463b
DeveloperAsik <firman.begin@gmail.com>
2025-04-11 10:48:28 +0700

    up: fixing auth middleware #8

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
+Route::get('/', 'App\Http\Controllers\Api\UserController@view')->name('api.user.view');
 Route::middleware(['auth'])->group(function ($e) {
     Route::get('/', function () {
         dd('iyey');
     });
-        Route::get('/api/lists', 'App\Http\Controllers\Api\UserController@view')->name('api.user.view');
+    Route::get('/api/lists', 'App\Http\Controllers\Api\UserController@view')->name('api.user.view');
     /*
      * 
      * extraweb routes start here
