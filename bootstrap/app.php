9f75e22a2cd09131259b9f64566396a3d4fb469b
DeveloperAsik <firman.begin@gmail.com>
2025-04-11 10:21:58 +0700

    up: fixing auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,14 +3,27 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\Authenticate;
 
 return Application::configure(basePath: dirname(__DIR__))->withRouting(
                 web: __DIR__ . '/../routes/web.php',
                 commands: __DIR__ . '/../routes/console.php',
                 health: '/up',
         )->withMiddleware(function (Middleware $middleware) {
-            $middleware->append(Authenticate::class);
+            $middleware->prepend(Authenticate::class);
+            $middleware->use([
+                \Illuminate\Session\Middleware\StartSession::class,
+                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+                \Illuminate\Http\Middleware\HandleCors::class,
+                \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+                \Illuminate\Http\Middleware\FrameGuard::class,
+                \Fruitcake\Cors\HandleCors::class,
+                \App\Http\Middleware\Cors::class,
+                \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+                \App\Http\Middleware\TrustProxies::class,
+                \App\Http\Middleware\TrimStrings::class,
+                \App\Http\Middleware\Authenticate
+            ]);
         })->withExceptions(function (Exceptions $exceptions) {
             //
         })->create();
